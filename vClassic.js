4a3df074354493394bee88ef086ec75122e63931
ammopy <amr.haleem@gmail.com>
2025-04-14 04:03:38 +0300

    Memory mgt. & UI/UX

--- vClassic.js
+++ vClassic.js
@@ -1,7 +1,7 @@
 /* ========== AUTO-GENERATED FROM vdata.js ========== */
 // For Service Workers and classic scripts
 self.AppVersions = {
-  LIB_V: '2.3',
+  LIB_V: '2.4',
   FFMPEG_BASE: 'js/ffmpeg',
   FFMPEG_V: '0.12.15',
   FFMPEG_C_V: '0.12.10'
--- versions.js
+++ versions.js
@@ -2,7 +2,7 @@
 // For ES Modules
 // Example usage: const { FFMPEG_BASE, FFMPEG_V } = VERSIONS;
 export const VERSIONS = {
-  LIB_V: '2.3',
+  LIB_V: '2.4',
   FFMPEG_BASE: 'js/ffmpeg',
   FFMPEG_V: '0.12.15',
   FFMPEG_C_V: '0.12.10'
